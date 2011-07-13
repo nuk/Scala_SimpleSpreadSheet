151e58ee90e227fe36ebcc307dedb7361cc20189
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2011-07-13 18:37:09 -0300

    Adicionada Células de Referência e de Texto

--- src/sse/SimpleSpreadSheet.scala
+++ src/sse/SimpleSpreadSheet.scala
@@ -8,34 +8,43 @@ class SimpleSpreadSheet {
   def setValue (point:(Char,Int),value:Cell) = {
     value.sse = this;
     this.cells += point -> value
+    this
   }
   
-  def getValue (point:(Char,Int)) = {
+  def getValue (point:(Char,Int)):AnyRef = {
     this.cells(point).output
   }
 }
 
 abstract class Cell {
   var sse:SimpleSpreadSheet = null
-  def output:Int
+  def output:AnyRef
 }
 
 class AbsoluteCell(var value:Int) extends Cell{
-  def output = value
+  def output = value.asInstanceOf[AnyRef]
 }
 
 class AddCell(var c1:(Char,Int), var c2:(Char,Int)) extends Cell{
-  def output = (sse.getValue(c1)+sse.getValue(c2))
+  def output = (sse.getValue(c1).asInstanceOf[Int]+sse.getValue(c2).asInstanceOf[Int]).asInstanceOf[AnyRef]
 }
 
 class SubCell(var c1:(Char,Int), var c2:(Char,Int)) extends Cell{
-	def output = (sse.getValue(c1)-sse.getValue(c2))
+	def output = (sse.getValue(c1).asInstanceOf[Int]-sse.getValue(c2).asInstanceOf[Int]).asInstanceOf[AnyRef]
 }
 
 class MultCell(var c1:(Char,Int), var c2:(Char,Int)) extends Cell{
-	def output = (sse.getValue(c1)*sse.getValue(c2))
+	def output = (sse.getValue(c1).asInstanceOf[Int]*sse.getValue(c2).asInstanceOf[Int]).asInstanceOf[AnyRef]
 }
 
 class DivCell(var c1:(Char,Int), var c2:(Char,Int)) extends Cell{
-	def output = (sse.getValue(c1)/sse.getValue(c2))
+	def output = (sse.getValue(c1).asInstanceOf[Int]/sse.getValue(c2).asInstanceOf[Int]).asInstanceOf[AnyRef]
+}
+
+class RefCell(var ref:(Char,Int)) extends Cell{
+	def output = sse.getValue(ref)
+}
+
+class TextCell(var value:String) extends Cell{
+	def output = value
 }
--- src/sse/TestSimpleSpreadSheet.scala
+++ src/sse/TestSimpleSpreadSheet.scala
@@ -25,7 +25,7 @@ class TestSimpleSpreadSheet{
 	def should_return_all_setted_cells_values: Unit ={
 		var sheet = new SimpleSpreadSheet
 		sheet.setValue(('B',15),9)
-		sheet.setValue(('Z',10),10)
+			 .setValue(('Z',10),10)
 		assertEquals(9,sheet.getValue(('B',15)))
 		assertEquals(10,sheet.getValue(('Z',10)))
 	}
@@ -34,8 +34,8 @@ class TestSimpleSpreadSheet{
 	def should_compute_a_simple_sum: Unit ={
 		var sheet = new SimpleSpreadSheet
 		sheet.setValue(('B',15),9)
-		sheet.setValue(('Z',10),10)
-		sheet.setValue(('A',1),new AddCell(('B',15),('Z',10)))
+			 .setValue(('Z',10),10)
+			 .setValue(('A',1),new AddCell(('B',15),('Z',10)))
 		assertEquals(9+10,sheet.getValue(('A',1)))
 	}
 	
@@ -43,8 +43,8 @@ class TestSimpleSpreadSheet{
 	def should_compute_a_simple_subtraction: Unit ={
 		var sheet = new SimpleSpreadSheet
 		sheet.setValue(('B',15),9)
-		sheet.setValue(('Z',10),10)
-		sheet.setValue(('A',1),new SubCell(('B',15),('Z',10)))
+			 .setValue(('Z',10),10)
+			 .setValue(('A',1),new SubCell(('B',15),('Z',10)))
 		assertEquals(9-10,sheet.getValue(('A',1)))
 	}
 	
@@ -52,8 +52,8 @@ class TestSimpleSpreadSheet{
 	def should_compute_a_simple_multiplication: Unit ={
 		var sheet = new SimpleSpreadSheet
 		sheet.setValue(('B',15),9)
-		sheet.setValue(('Z',10),10)
-		sheet.setValue(('A',1),new MultCell(('B',15),('Z',10)))
+			 .setValue(('Z',10),10)
+			 .setValue(('A',1),new MultCell(('B',15),('Z',10)))
 		assertEquals(9*10,sheet.getValue(('A',1)))
 	}
 	
@@ -61,8 +61,8 @@ class TestSimpleSpreadSheet{
 	def should_compute_a_simple_division: Unit ={
 		var sheet = new SimpleSpreadSheet
 		sheet.setValue(('B',15),8)
-		sheet.setValue(('Z',10),2)
-		sheet.setValue(('A',1),new DivCell(('B',15),('Z',10)))
+			 .setValue(('Z',10),2)
+			 .setValue(('A',1),new DivCell(('B',15),('Z',10)))
 		assertEquals(8/2,sheet.getValue(('A',1)))
 	}
 	
@@ -70,14 +70,14 @@ class TestSimpleSpreadSheet{
 	def should_compute_a_complex_calculation: Unit ={
 		var sheet = new SimpleSpreadSheet
 		sheet.setValue(('A',1),5)
-		sheet.setValue(('B',1),2)
-		sheet.setValue(('C',1),new AddCell(('A',1),('B',1)))
-		sheet.setValue(('D',1),3)
-		sheet.setValue(('E',1),new SubCell(('C',1),('D',1)))
-		sheet.setValue(('F',1),4)
-		sheet.setValue(('G',1),new MultCell(('E',1),('F',1)))
-		sheet.setValue(('H',1),2)
-		sheet.setValue(('I',1),new DivCell(('G',1),('H',1)))
+			 .setValue(('B',1),2)
+			 .setValue(('C',1),new AddCell(('A',1),('B',1)))
+			 .setValue(('D',1),3)
+			 .setValue(('E',1),new SubCell(('C',1),('D',1)))
+			 .setValue(('F',1),4)
+			 .setValue(('G',1),new MultCell(('E',1),('F',1)))
+			 .setValue(('H',1),2)
+			 .setValue(('I',1),new DivCell(('G',1),('H',1)))
 		assertEquals((((5+2)-3)*4)/2,sheet.getValue(('I',1)))
 	}
 	
@@ -85,16 +85,31 @@ class TestSimpleSpreadSheet{
 	def should_compute_a_complex_calculation_changing_values: Unit ={
 		var sheet = new SimpleSpreadSheet
 		sheet.setValue(('A',1),5)
-		sheet.setValue(('B',1),2)
-		sheet.setValue(('C',1),new AddCell(('A',1),('B',1)))
-		sheet.setValue(('D',1),3)
-		sheet.setValue(('E',1),new SubCell(('C',1),('D',1)))
-		sheet.setValue(('F',1),4)
-		sheet.setValue(('G',1),new MultCell(('E',1),('F',1)))
-		sheet.setValue(('H',1),2)
-		sheet.setValue(('I',1),new DivCell(('G',1),('H',1)))
+			 .setValue(('B',1),2)
+			 .setValue(('C',1),new AddCell(('A',1),('B',1)))
+			 .setValue(('D',1),3)
+			 .setValue(('E',1),new SubCell(('C',1),('D',1)))
+			 .setValue(('F',1),4)
+			 .setValue(('G',1),new MultCell(('E',1),('F',1)))
+			 .setValue(('H',1),2)
+			 .setValue(('I',1),new DivCell(('G',1),('H',1)))
 		assertEquals((((5+2)-3)*4)/2,sheet.getValue(('I',1)))
 		sheet.setValue(('D',1),7)
 		assertEquals((((5+2)-7)*4)/2,sheet.getValue(('I',1)))
 	}
+	
+	@Test
+	def should_compute_a_reference_cell: Unit ={
+	  var sheet = new SimpleSpreadSheet
+		sheet.setValue(('A',1),5)
+			 .setValue(('B',1),new RefCell(('A',1)))
+		assertEquals(5,sheet.getValue(('B',1)))
+	}
+	
+	@Test
+	def should_compute_a_text_cell: Unit ={
+	  var sheet = new SimpleSpreadSheet
+	  sheet.setValue(('A',1),new TextCell("hello world"))
+	  assertEquals("hello world",sheet.getValue(('A',1)))
+  }
 }
